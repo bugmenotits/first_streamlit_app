4cd4ecf3eabdfdf95a1596054ce06560fee96466
bugmenotits <37191565+bugmenotits@users.noreply.github.com>
2022-12-29 10:35:14 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,6 +16,6 @@ df = pd.read_csv('https://uni-lab-files.s3.us-west-2.amazonaws.com/dabw/fruit_ma
 streamlit.dataframe(df)
 
 df = df.set_index('Fruit')
-streamlit.multiselect('Pick', list(df))
+streamlit.multiselect('Pick', list(df.index),['Avocado','Strawberries'])
 
 streamlit.dataframe(df)
