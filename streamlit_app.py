aa11d3a571d29b1347a21ed2b94ad3376cfa51bc
bugmenotits <37191565+bugmenotits@users.noreply.github.com>
2022-12-29 10:26:51 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,3 +5,8 @@ streamlit.title('first ever')
 
 streamlit.text('testing')
 streamlit.text('testing2')
+
+streamlit.header('Breakfast Menu')
+streamlit.text('Omega 3 & Blueberry Oatmeal')
+streamlit.text('Kale, Spinach & Rocket Smoothie')
+streamlit.text('Hard-Boiled Free-Range Egg')
